a478afbbbcc008650818cc5dc90b2064e9e35cfb
Burak <kelesburak13@outlook.com>
2019-01-23 14:50:37 +0300

    First Project
    Change image with button (just one time)

--- MyFirstApplication/app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ MyFirstApplication/app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myfirstapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    public void changeImage (View view){
+
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.barong_neon);
+
+    }
+
 }
